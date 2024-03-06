818e81be13604f501b7c5414d39da5e5d097c5ac
cyrusc2026 <cyrusc2026@student.cis.edu.hk>
2024-03-06 08:22:54 +0800

    Commit 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,19 +3,19 @@ public class Main {
     public static void main(String[] args) {
 
         // call the first add method and store the result into a variable called resultOne.
-
+int resultOne = add(5, 6);
         // print resultOne.
-
+System.out.println (resultOne);
         // call the greeting method and store the result into a variable called resultTwo.
-
+String resultTwo = greeting("Cyrus");
         // print resultTwo.
-
+        System.out.println (resultTwo);
         // call the second add method and store the result into a variable called resultThree.
-
+int resultThree = add(1,2,3,4);
         // print resultThree.
-
+System.out.println (resultThree);
         // call the printCapitalised method.
-
+printCapitalised("abcdefg");
     }
 
     /**
@@ -24,6 +24,9 @@ public class Main {
      * @param numTwo - an integer
      * @return the sum of numOne and numTwo (an integer)
      */
+    public static int add(int numOne, int numTwo){
+        return numOne + numTwo;
+    }
 
 
     /**
@@ -31,7 +34,9 @@ public class Main {
      * @param name - the name of a person (a String)
      * @return a greeting in the format of "Bonjour, [name]!" (a String)
      */
-
+public static String greeting(String name){
+    return ("Bonjour, " + name + "!");
+}
 
     /**
      * add
@@ -41,13 +46,20 @@ public class Main {
      * @param numFour - an integer
      * @return the sum of numOne, numTwo, numThree, and numFour (an integer)
      */
-
+public static int add (int numOne, int numTwo, int numThree, int numFour){
+    int sumTwo = add(numOne, numTwo);
+    int sumThree = add(numThree, sumTwo);
+    int sumFour = add(sumThree, numFour);
+    return sumFour;
+}
 
     /**
      * printCapitalised
-     * @param toBePrinted - a String that the user wishes to be printed in all caps.
+     * @param tobePrinted - a String that the user wishes to be printed in all caps.
      */
-
+public static void printCapitalised (String tobePrinted){
+    System.out.println(tobePrinted.toUpperCase());
+}
 
 
 
